e0056eb6d8eff8c06f2a76c4ec7bb744b7fe6e54
78-quangSky <thien.dangquang.sistrain@gmail.com>
2024-02-23 15:34:29 +0700

    .

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fablab_lib',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     install_requires=install_requires,
     author='FabLab Innovation',
